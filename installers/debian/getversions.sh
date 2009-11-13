beb9cbb30e0685d7624329b08b51e5e1a9692bd3
John 'Warthog9' Hawley <warthog9@eaglescrag.net>
2009-11-12 17:32:14 -0800

    Installers - Debian: Small adjustments makine multiple make runs not clober
    There was an issue that running multiple makes back to back caused the paths
    to get clobber in a bad way (I.E. unusable)

--- installers/debian/getversions.sh
+++ installers/debian/getversions.sh
@@ -9,7 +9,9 @@ do
 	eval url=\$$ageclass
 	
 	list="$( perl getlinks.pl "$url" | sed 's/\/$//gi' )"
-	list=$( echo "${list}" | grep -vi "kernel\|index\|build\|dostools\|graphics\|debuginfo\|\?\|newbb\|readme\|HEADER\|RPM-GPG-KEY-beta\|TIME\|timestamp.txt\|beta\|test\|linux\|updates\|development\|releases\|debian-archive" )
+	list=$( echo "${list}" | grep -vi "kernel\|index\|build\|dostools\|graphics\|debuginfo\|\?\|newbb\|readme\|HEADER\|RPM-GPG-KEY-beta\|TIME\|timestamp.txt\|beta\|test\|linux\|updates\|development\|releases\|debian-archive\|experimental" )
+	# This is done because the following releases don't contain the right information for network instalation that I know of:
+	list=$( echo "${list}" | grep -vi "0.93R6\|Debian-1.1\|buzz\|Debian-1.2\|rex\|Debian-1.3.1\|bo\|Debian-2.0\|hamm\|Debian-2.1\|slink\|Debian-2.2\|potato\|Debian-3.0\|woody" )
 
 	case "${ageclass}" in
 		ARCHIVEURL)
@@ -38,5 +40,5 @@ done
 
 echo "ALLVERS = ${ALLVERS}"
 echo "ALLVERS_32bit = ${ALLVERS}"
-echo "ALLVERS_64bit = $(echo "${ALLVERS}" | sed 's/\s1\s/ /' )"
+echo "ALLVERS_64bit = $(echo "${ALLVERS}" | sed -e 's/\s1\s/ /' -e 's/Debian-3.1//g' -e 's/sarge//g' )"
 
