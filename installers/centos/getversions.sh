96825178d9609a80f4286437d8d22394b6128d15
John 'Warthog9' Hawley <warthog9@eaglescrag.net>
2012-09-07 11:35:22 -0600

    installers/centos - Auto discovery of EOLed versions
    This should add more automatic discovery of EOLed versions, thus
    meaning the makefiles should work even when these haven't been updated
    to reflect the EOLed nature

--- installers/centos/getversions.sh
+++ installers/centos/getversions.sh
@@ -5,6 +5,26 @@ CURRENT=""
 VAULT=""
 ANCIENT=""
 
+# Get EOL versions:
+# Create list of EOLed versions:
+eol_vers=""
+
+eol_list="$( perl getlinks.pl "${CURRENTURL}" | sed 's/\/$//gi' | grep "^[0-9]" )"
+
+for major_ver in $( echo "${eol_list}" | tr " " "\n" | awk 'BEGIN { FS="."; } ; { print $1; }' | sort -u )
+do
+	found_readme=""
+	found_readme="$( perl getlinks.pl "${CURRENTURL}/${major_ver}" |  grep -v "^\?" | grep -i "readme" )"
+	if [[ ! -z "${found_readme}" ]]
+	then
+		eol_vers="$( echo "${eol_vers} ${major_ver}" | sed -e 's/^ *//g' -e 's/ *$//g' )"
+	fi
+done
+eol_vers_grep="$( echo "${eol_vers}" | tr " " "\n" | sed 's/^/\^/g' | tr "\n" " " | sed -e 's/^ *//g' -e 's/  *$//g' -e 's/ /\\|/g' )"
+
+#echo "Centos EOL Vers: ${eol_vers}"
+#echo "Centos EOL Vers Grep: ${eol_vers_grep}"
+
 for ageclass in ANCIENTURL VAULTURL CURRENTURL
 do
 	case "${ageclass}" in
@@ -12,6 +32,7 @@ do
 		VAULTURL) url=${VAULTURL} ;;
 		CURRENTURL) url=${CURRENTURL} ;;
 	esac
+
 	
 	list="$( perl getlinks.pl "$url" | sed 's/\/$//gi' | grep "^[0-9]" )"
 	list=$( echo "${list}" | grep -vi "Name\|Last modified\|Size\|Parent Directory\|beta" )
@@ -22,14 +43,30 @@ do
 			ANCIENT="$( echo "$list" | grep "^2" | tr "\n" " " )"
 			;;
 		VAULTURL)
-			VAULT="$( echo "$list" | grep -v "^2" |  tr "\n" " " )"
+			# create "latest" versions to ignore in vault
+			# 2 should always be ignored, it's ancient
+
+			# Now figure out what's left
+
+			last_vers=""
+			major_vers="$( echo "${list}" | tr " " "\n" | awk 'BEGIN { FS="."; } ; { print $1; }' | sort -u | grep -v "${eol_vers_grep}" | tr "\n" " ")"
+			#echo "Major Vers: ${major_vers}"
+			for major_ver in ${major_vers}
+			do
+				last_vers="$( echo "${last_vers} $( echo "${list}" | grep "^${major_ver}" | tail -n 1 )" | tr " " "\n" | sort -u | tr "\n" " " | sed -e 's/^ *//g' -e 's/ *$//g' )"
+			done
+			#echo "Last Vers: ${last_vers}"
+			last_vers_grep="$( echo "${last_vers}" | sed -e 's/ /\\|/g' )"
+			#echo "Last Vers Grep: ${last_vers_grep}"
+			VAULT="$( echo "$list" | grep -v "^2" | grep -v "${last_vers_grep}" |  tr "\n" " " )"
 			;;
 		CURRENTURL)
-			CURRENT="$( echo "$list" | grep -v "^3\|^2\|$(echo "${VAULT}" | sed 's/\s/\\|/gi' | sed 's/\\|$//' )" | tr "\n" " " )"
+			CURRENT="$( echo "$list" | grep -v "${eol_vers_grep}\|$(echo "${VAULT}" | sed 's/\s/\\|/gi' | sed 's/\\|$//' )" | tr "\n" " " )"
 			;;
 	esac
 done
 
+echo "EOL = ${eol_vers}"
 echo "ANCIENT = ${ANCIENT}"
 echo "VAULT = ${VAULT}"
 echo "CURRENT = ${CURRENT}"
